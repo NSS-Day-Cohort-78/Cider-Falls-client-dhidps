0d9a85e537b99abbe093d62c4879bfac113a3a4c
Evan Alford <evan.al4d@gmail.com>
2025-08-05 15:16:58 -0500

    made contact function

--- scripts/Contact.js
+++ scripts/Contact.js
@@ -1,3 +1,11 @@
 // export contact function
-    // generate contactHTML
+export const contact = () => {
+    let contactHTML = "";
+        contactHTML += `
+             <footer> Phone Number: 615-733-8472 |  Email: Ciderfallspark@Ciderfalls.com  |  Address: 789 Creekside Lane
+            Murfreesboro TN 37130 </footer>`
+// generate contactHTML
     // return contactHTML
+    return contactHTML
+}
+    
