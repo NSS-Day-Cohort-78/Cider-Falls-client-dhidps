fba22ab270ac4c4d738d99181a12452527c38ba9
Evan Alford <evan.al4d@gmail.com>
2025-08-05 15:25:21 -0500

    revise contact function

--- scripts/Contact.js
+++ scripts/Contact.js
@@ -1,11 +1,12 @@
 // export contact function
 export const contact = () => {
+    // generate contactHTML
     let contactHTML = "";
         contactHTML += `
-             <footer> Phone Number: 615-733-8472 |  Email: Ciderfallspark@Ciderfalls.com  |  Address: 789 Creekside Lane
-            Murfreesboro TN 37130 </footer>`
-// generate contactHTML
+             <p> Phone Number: 615-733-8472 </p>  <p> Email: Ciderfallspark@Ciderfalls.com </p>    <p> Address: 789 Creekside Lane 
+            Murfreesboro TN 37130 </p>`
+            
     // return contactHTML
-    return contactHTML
+return contactHTML
 }
     
