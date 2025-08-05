7ce79371c36278f65796b0a518770cb0bf1862f9
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-05 16:03:35 -0500

    did parkareas

--- scripts/Contact.js
+++ scripts/Contact.js
@@ -1,12 +1,11 @@
 // export contact function
 export const contact = () => {
     // generate contactHTML
-    let contactHTML = "";
-        contactHTML += `
+    let contactHTML = ""
+    contactHTML += `
              <p> Phone Number: 615-733-8472 </p>  <p> Email: Ciderfallspark@Ciderfalls.com </p>    <p> Address: 789 Creekside Lane 
             Murfreesboro TN 37130 </p>`
-            
+
     // return contactHTML
-return contactHTML
+    return contactHTML
 }
-    
--- scripts/ParkAreas.js
+++ scripts/ParkAreas.js
@@ -1,11 +1,32 @@
 // import database.js
+import { parkAreasData } from "./database.js"
 // import Services.js
+import { services } from "./Services.js"
 
 //export parkAreas function
-    // call copy of park areas array and store in variable
-    // parkAreaHTML
-    // loop park area data
-        // get serviceId array
-        // call Services.js function stored in a variable using serviceId array as argument
-        // add HTML to parkAreaHTML for each park
-    // return parkAreaHTML
+export const parkAreas = () => {
+    const parks = parkAreasData()
+
+    let html = ""
+
+    for (const park of parks) {
+        const serviceArr = park.serviceId
+
+        services(serviceArr)
+
+        html += `
+        <article class=''>
+            <h2>${park.title}</h2>
+            ${services}
+        </article>
+        `
+    }
+    return html
+}
+// call copy of park areas array and store in variable
+// parkAreaHTML
+// loop park area data
+// get serviceId array
+// call Services.js function stored in a variable using serviceId array as argument
+// add HTML to parkAreaHTML for each park
+// return parkAreaHTML
