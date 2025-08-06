4247756354f80316e77395f9e4bc3c53a1a710c1
Evan Alford <evan.al4d@gmail.com>
2025-08-06 09:56:38 -0500

    completed guest revised

--- scripts/Guests.js
+++ scripts/Guests.js
@@ -1,15 +1,18 @@
 // import database.js
-import database from {database}
+import {guestsData} from "./database.js"
 // export guests function
 export const guests = () => {
  // call copy of guest array and store in variable
-    guestData()
-    // guestsHTML
-    let guestsHTML = "";
-        for (const guest of database.guests) {
-            guestsHTML += `<ul> ${guest.firstName}, ${guest.lastName} </ul>`
+    const guestDataCopy = guestsData()
+    // guestsHTML 
+    
+    let guestsHTML = "<ul>";
+        for (const guest of guestDataCopy) {
+            guestsHTML += `<li>${guest.firstName}, ${guest.lastName}</li>`
             
         }
+        guestsHTML += "</ul>"
+
 
     // loop guests
         // make HTML and add to guestsHTML
