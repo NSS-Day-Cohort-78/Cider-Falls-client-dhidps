0c774433599d67f5e524172e6f5b6115f138d5b2
Evan Alford <evan.al4d@gmail.com>
2025-08-06 09:26:15 -0500

    completed contact

--- scripts/Guests.js
+++ scripts/Guests.js
@@ -1,8 +1,19 @@
 // import database.js
-
+import database from {database}
 // export guests function
-    // call copy of guest array and store in variable
+export const guests = () => {
+ // call copy of guest array and store in variable
+    guestData()
     // guestsHTML
+    let guestsHTML = "";
+        for (const guest of database.guests) {
+            guestsHTML += `<ul> ${guest.firstName}, ${guest.lastName} </ul>`
+            
+        }
+
     // loop guests
         // make HTML and add to guestsHTML
     // return guestsHTML
+    return guestsHTML
+}
+   
